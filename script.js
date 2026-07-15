f93e6679cbf5e6eaf4d60c31289d22c05d657445
JSSRuiz19 <jsamuelsruiz1@gmail.com>
2026-07-14 23:02:47 -0500

    Added second button and messages

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const button = document.getElementById("beginBtn");
 
+const hero = document.getElementById("hero");
+
+const letter = document.getElementById("letter");
+
+let currentPage = 0;
+
+const nextBtn = document.getElementById("nextBtn");
+
 button.addEventListener("click", () => {
+        hero.style.display = "none";
+        letter.style.display = "flex";
+    });
 
-    alert("Welcome to your birthday surprise! 🎉");
-
+nextBtn.addEventListener("click", () => {
+    const current = document.getElementById("page" + currentPage);
+    current.style.display = "none";
+    currentPage++;
+    const next = document.getElementById("page" + currentPage);
+    next.style.display = "block";
 });
