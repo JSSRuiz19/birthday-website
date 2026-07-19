4a6e4d6e14ffdee2e5c1fcb864ce175048911bd6
JSSRuiz19 <jsamuelsruiz1@gmail.com>
2026-07-18 22:27:58 -0500

    added stickers

--- script.js
+++ script.js
@@ -8,7 +8,93 @@ let currentPage = 0;
 
 const nextBtn = document.getElementById("nextBtn");
 
+const ending = document.getElementById("ending");
 
+const typewriter = document.getElementById("typewriter");
+
+const endingText =
+`Thank you for taking your time to read this. 
+I hope this made you smile and feel special. 
+Remember that I'll always be here for you if you ever need anything. 
+Again, sorry for the hersey bar 🍫
+
+🌹
+-Joseph`;
+
+function typeMessage(text){
+
+    typewriter.textContent = "";
+
+    let i = 0;
+
+    const timer = setInterval(() =>{
+
+        typewriter.textContent += text.charAt(i);
+
+        i++;
+
+        if(i >= text.length){
+
+            clearInterval(timer);
+
+        }
+
+    },50);
+
+}
+
+
+const pageStickers = [
+
+    [
+        {src:"1x/NurseStickers/Badge.png", top:"8%", left:"5%"},
+        {src:"1x/NurseStickers/HeartTool.png", top:"70%", right:"5%"}
+    ],
+
+    [
+        {src:"1x/NurseStickers/Lanyard.png", top:"12%", right:"8%"},
+        {src:"1x/NurseStickers/Medicine.png", bottom:"10%", left:"6%"}
+    ],
+
+    [
+        {src:"1x/NurseStickers/Nurse pen.png", top:"15%", left:"8%"},
+        {src:"1x/NurseStickers/Pressure.png", bottom:"12%", right:"8%"}
+    ],
+
+    [
+        {src:"1x/NurseStickers/Scrub.png", top:"8%", right:"6%"},
+        {src:"1x/NurseStickers/Shot.png", bottom:"8%", left:"5%"}
+    ],
+
+    [
+        {src:"1x/NurseStickers/Therm.png", top:"10%", left:"5%"},
+        {src:"1x/NurseStickers/Writing.png", bottom:"10%", right:"5%"}
+    ]
+
+];
+
+
+function showStickers(page){
+
+    const container = document.getElementById("stickerContainer");
+
+    container.innerHTML = "";
+
+    pageStickers[page].forEach(sticker =>{
+
+        const img = document.createElement("img");
+
+        img.src = sticker.src;
+
+        img.classList.add("sticker");
+
+        Object.assign(img.style, sticker);
+
+        container.appendChild(img);
+
+    });
+
+}
 
 const startBtn = document.getElementById("startBtn");
 
@@ -19,31 +105,43 @@ startBtn.addEventListener("click", () => {
 
 button.addEventListener("click", () => {
         hero.style.display = "none";
-
         letter.style.display = "flex";
+        showStickers(0);
     });
 
 nextBtn.addEventListener("click", () => {
-    
-    if (currentPage < 4){
 
-        const current = document.getElementById("page" + currentPage);
-        current.style.opacity = "0";
+    // If we're already on the last page,
+    // go to the ending.
+    if(currentPage === 4){
+
+        letter.style.display = "none";
+        ending.style.display = "flex";
+
+        typeMessage(endingText);
+
+        return;
+    }
+
+    const current = document.getElementById("page" + currentPage);
+    current.style.opacity = "0";
+
+    setTimeout(() => {
+
+        current.style.display = "none";
+
+        currentPage++;
+
+        const next = document.getElementById("page" + currentPage);
+
+        next.style.display = "block";
+
+        showStickers(currentPage);
 
         setTimeout(() => {
-            current.style.display = "none";
+            next.style.opacity = "1";
+        }, 10);
 
-            currentPage++;
-
-            const next = document.getElementById("page" + currentPage);
-            next.style.display = "block";
-
-            setTimeout(() => {
-                next.style.opacity = "1";
-            }, 10);
-
-        }, 500);
-
-    }
+    }, 500);
 
 });
